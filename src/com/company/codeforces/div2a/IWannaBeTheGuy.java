208171eedd1edd5ec8cebb7eaeeea9faf4cbc783
Abhyudit Jain <abhyuditjain@gmail.com>
2018-02-14 17:28:28 +0530

    I wanna be the guy

---
+++ src/com/company/codeforces/div2a/IWannaBeTheGuy.java
@@ -0,0 +1,47 @@
+package com.company.codeforces.div2a;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * There is a game called "I Wanna Be the Guy", consisting of n levels. Little X and his friend Little Y are addicted to the game. Each of them wants to pass the whole game.
+ * <p>
+ * Little X can pass only p levels of the game. And Little Y can pass only q levels of the game. You are given the indices of levels Little X can pass and the indices of levels Little Y can pass. Will Little X and Little Y pass the whole game, if they cooperate each other?
+ * <p>
+ * Input
+ * The first line contains a single integer n (1 ≤  n ≤ 100).
+ * <p>
+ * The next line contains an integer p (0 ≤ p ≤ n) at first, then follows p distinct integers a1, a2, ..., ap (1 ≤ ai ≤ n). These integers denote the indices of levels Little X can pass. The next line contains the levels Little Y can pass in the same format. It's assumed that levels are numbered from 1 to n.
+ * <p>
+ * Output
+ * If they can pass all the levels, print "I become the guy.". If it's impossible, print "Oh, my keyboard!" (without the quotes).
+ */
+
+public class IWannaBeTheGuy {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+
+        Set<Integer> set = new HashSet<>();
+
+        int px = scanner.nextInt();
+
+        for (int i = 0; i < px; i++) {
+            set.add(scanner.nextInt());
+        }
+
+        int py = scanner.nextInt();
+
+        for (int i = 0; i < py; i++) {
+            set.add(scanner.nextInt());
+        }
+
+        if (set.size() == n) {
+            System.out.println("I become the guy.");
+        } else {
+            System.out.println("Oh, my keyboard!");
+        }
+    }
+}
